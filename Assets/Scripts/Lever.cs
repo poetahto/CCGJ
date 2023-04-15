ba779224d471b53ffcb2add856eb5dc10e4d6ab7
Daniel Walls <walls.dk@gmail.com>
2023-04-15 12:45:02 -0700

    lever

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,50 @@
+﻿using poetools.player.Player.Interaction;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+    public class Lever : NetworkBehaviour, IInteractable
+    {
+        [SerializeField] private bool defaultValue;
+        [SerializeField] private UnityEvent flippedOn;
+        [SerializeField] private UnityEvent flippedOff;
+
+        private NetworkVariable<bool> _isFlipped = new NetworkVariable<bool>();
+
+        public override void OnNetworkSpawn()
+        {
+            _isFlipped.OnValueChanged += HandleValueChanged;
+
+            if (IsServer)
+                _isFlipped.Value = defaultValue;
+
+            HandleValueChanged(default, _isFlipped.Value);
+        }
+
+        private void HandleValueChanged(bool previousValue, bool newValue)
+        {
+            if (newValue)
+                flippedOn.Invoke();
+
+            else flippedOff.Invoke();
+        }
+
+        public void HandleInteractStart(GameObject grabber)
+        {
+            SetIsFlippedServerRpc(!_isFlipped.Value);
+        }
+
+        public void HandleInteractStop(GameObject grabber)
+        {
+            // do nothing
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void SetIsFlippedServerRpc(bool value)
+        {
+            _isFlipped.Value = value;
+        }
+    }
+}
