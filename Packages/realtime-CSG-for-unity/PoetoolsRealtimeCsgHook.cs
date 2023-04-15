d04cfe1ee00a30e377943fac0fb3765b15e126f0
Daniel Walls <walls.dk@gmail.com>
2023-04-14 21:11:48 -0700

    realtime csg

---
+++ Packages/realtime-CSG-for-unity/PoetoolsRealtimeCsgHook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using InternalRealtimeCSG;
+using RealtimeCSG;
+using UnityEngine;
+
+public static class PoetoolsRealtimeCsgHook
+{
+    public static void HandleRebuild()
+    {
+        var settings = Resources.Load<PoetoolsRealtimeCsgSettings>("CSG Settings");
+
+        Debug.Log("Poetools custom logic!");
+
+        foreach (var model in CSGModelManager.GetAllModels())
+        {
+            GeneratedMeshInstance[] meshes = MeshInstanceManager.GetAllModelMeshInstances(model.generatedMeshes);
+
+            if (meshes != null)
+            {
+                foreach (var mesh in meshes)
+                {
+                    if (mesh.RenderSurfaceType == RenderSurfaceType.Collider && mesh.gameObject.activeSelf)
+                        mesh.gameObject.SetActive(false);
+
+                    if (mesh.RenderSurfaceType == RenderSurfaceType.Normal)
+                        mesh.gameObject.AddComponent<MeshCollider>();
+
+                    if (mesh.RenderSurfaceType == RenderSurfaceType.Normal)
+                    {
+                        if (_tagLookup.TryGetValue(mesh.RenderMaterial, out var tag))
+                            mesh.gameObject.AddTags(tag);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Packages/realtime-CSG-for-unity/PoetoolsRealtimeCsgSettings.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using poetools.Core;
+using poetools.Core.Dictionary;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu]
+public class PoetoolsRealtimeCsgSettings : ScriptableObject
+{
+    public struct Settings
+    {
+        public Mat
+    }
+    public List<>
+}
+
+public class PoetoolsRealtimeCsgLogic
+{
+
+}
