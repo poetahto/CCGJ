2c013aa0d87701f3a1a9821c9a81512e71dd053b
Daniel Walls <walls.dk@gmail.com>
2023-04-15 11:50:49 -0700

    acceptors and toggle objects

---
+++ Assets/Scripts/Acceptor.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using poetools.Core;
+using poetools.Core.Abstraction;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+    public class Acceptor : TriggerEffect
+    {
+        public List<NetworkObject> validObjects;
+        public UnityEvent onAccept;
+        public UnityEvent onRelease;
+
+        public NetworkObject AcceptedObject { get; private set; }
+
+        protected override void HandleCollisionEnter(GameObject obj)
+        {
+            if (obj.TryGetComponent(out NetworkObject netObj) && validObjects.Contains(netObj))
+            {
+                var grav = netObj.GetComponentInChildren<Gravity>();
+
+                if (grav)
+                    grav.enabled = false;
+
+                AcceptedObject = netObj;
+                onAccept.Invoke();
+            }
+        }
+
+        protected override void HandleCollisionExit(GameObject obj)
+        {
+            if (obj.TryGetComponent(out NetworkObject netObj) && netObj == AcceptedObject)
+            {
+                var grav = netObj.GetComponentInChildren<Gravity>();
+
+                if (grav)
+                    grav.enabled = true;
+
+                AcceptedObject = null;
+                onRelease.Invoke();
+            }
+        }
+
+        [SerializeField]
+        private float movementSpeed = 1;
+        [SerializeField]
+        private float maxSpeed = 1;
+
+        private void FixedUpdate()
+        {
+            if (AcceptedObject != null && AcceptedObject.IsOwner)
+            {
+                // AcceptedObject.transform.position = Vector3.MoveTowards(AcceptedObject.transform.position,
+                //     transform.position, Time.deltaTime);
+                var Rigidbody = AcceptedObject.GetComponent<Rigidbody>();
+
+                Vector3 target = transform.position;
+                Vector3 current = AcceptedObject.transform.position;
+                Vector3 directionToTarget = target - current;
+
+                Rigidbody.velocity = directionToTarget * movementSpeed / Time.fixedDeltaTime;
+                Rigidbody.velocity = Vector3.ClampMagnitude(Rigidbody.velocity, maxSpeed);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ToggleObject.cs
@@ -0,0 +1,44 @@
+﻿using Unity.Netcode;
+
+namespace DefaultNamespace
+{
+
+    public class ToggleObject : NetworkBehaviour
+    {
+        public NetworkVariable<bool> isOn = new NetworkVariable<bool>();
+        public bool defaultValue;
+
+        public override void OnNetworkSpawn()
+        {
+            isOn.OnValueChanged += HandleValueChanged;
+
+            if (IsServer)
+                isOn.Value = defaultValue;
+
+            HandleValueChanged(default, isOn.Value);
+        }
+
+        private void HandleValueChanged(bool oldValue, bool newValue)
+        {
+            // todo: replace w/ cooler effects
+            gameObject.SetActive(newValue);
+        }
+
+        public void ToggleOn()
+        {
+            SetIsOnServerRpc(true);
+        }
+
+        public void ToggleOff()
+        {
+            SetIsOnServerRpc(false);
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void SetIsOnServerRpc(bool value)
+        {
+            isOn.Value = value;
+        }
+
+    }
+}
