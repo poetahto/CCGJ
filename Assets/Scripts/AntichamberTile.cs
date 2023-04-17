3e8cd0ac99e4c6209e4bc1084f5cbfc9f572ca28
Daniel Walls <walls.dk@gmail.com>
2023-04-16 18:07:09 -0700

    started stuff

---
+++ Assets/Scripts/AntichamberTile.cs
@@ -0,0 +1,39 @@
+﻿using ElRaccoone.Tweens;
+using poetools.player.Player.Interaction;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class AntiChamberTile : MonoBehaviour, IInteractable
+    {
+        public CanvasGroup imageGroup;
+        public CanvasGroup textGroup;
+        public float duration = 0.5f;
+
+        private bool _showingImage;
+
+        public void HandleInteractStart(GameObject grabber)
+        {
+            if (_showingImage)
+                ShowText();
+
+            else ShowImage();
+        }
+
+        private void ShowImage()
+        {
+
+        }
+
+        private void ShowText()
+        {
+            textGroup.TweenCancelAll();
+            textGroup.TweenCanvasGroupAlpha(1, duration);
+        }
+
+        public void HandleInteractStop(GameObject grabber)
+        {
+            // Not needed.
+        }
+    }
+}
