394ae680438fc7ef3a422205829ef7d0595733fa
Daniel Walls <walls.dk@gmail.com>
2023-04-16 18:44:32 -0700

    more stuff

--- Assets/Scripts/AntichamberTile.cs
+++ Assets/Scripts/AntichamberTile.cs
@@ -1,6 +1,9 @@
-﻿using ElRaccoone.Tweens;
+﻿using System;
+using ElRaccoone.Tweens;
 using poetools.player.Player.Interaction;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DefaultNamespace
 {
@@ -8,9 +11,20 @@ namespace DefaultNamespace
     {
         public CanvasGroup imageGroup;
         public CanvasGroup textGroup;
+        public Sprite sprite;
+        [TextArea]
+        public string message;
         public float duration = 0.5f;
 
         private bool _showingImage;
+
+        private void Awake()
+        {
+            textGroup.alpha = 0;
+            imageGroup.alpha = 1;
+            GetComponentInChildren<TMP_Text>().text = message;
+            GetComponentInChildren<Image>().sprite = sprite;
+        }
 
         public void HandleInteractStart(GameObject grabber)
         {
@@ -22,13 +36,16 @@ namespace DefaultNamespace
 
         private void ShowImage()
         {
-
+            textGroup.TweenCancelAll();
+            textGroup.TweenCanvasGroupAlpha(0, duration);
+            imageGroup.TweenCanvasGroupAlpha(1, duration);
         }
 
         private void ShowText()
         {
             textGroup.TweenCancelAll();
             textGroup.TweenCanvasGroupAlpha(1, duration);
+            imageGroup.TweenCanvasGroupAlpha(0, duration);
         }
 
         public void HandleInteractStop(GameObject grabber)
